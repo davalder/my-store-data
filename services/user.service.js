3130fffe823b224321d8ba9b78026af0a5d0cc0e
davalder <davalder@hotmail.com>
2022-07-18 22:41:10 -0500

    Implementando hashing para usuarios

--- services/user.service.js
+++ services/user.service.js
@@ -1,5 +1,6 @@
 const pool = require('../libs/postgres.pool');
 const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
 
 const { models } = require('./../libs/sequelize');
 
@@ -10,20 +11,25 @@ class UserService {
   }
 
   async create(data) {
-    const newUser = await models.User.create(data);
+    const hash = await bcrypt.hash(data.password, 10);
+    const newUser = await models.User.create({
+      ...data,
+      password: hash,
+    });
+    delete newUser.dataValues.password;
     return newUser;
   }
 
   async find() {
     const rta = await models.User.findAll({
-      include: ['customer']
+      include: ['customer'],
     });
     return rta;
   }
 
   async findOne(id) {
     const user = await models.User.findByPk(id);
-    if(!user) {
+    if (!user) {
       throw boom.notFound('User not found');
     }
     return user;
@@ -37,7 +43,7 @@ class UserService {
 
   async delete(id) {
     const user = await this.findOne(id);
-    await user.destroy()
+    await user.destroy();
     return { id };
   }
 }
