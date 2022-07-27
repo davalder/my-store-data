043a553a4ee273b4e0d951e78becd17939d95ca6
davalder <davalder@hotmail.com>
2022-07-26 23:31:16 -0500

    Validando tokens para cambio de contraseña

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -12,7 +12,7 @@ router.post(
   async (req, res, next) => {
     try {
       const user = req.user;
-      res.json(service.signToken(user))
+      res.json(service.signToken(user));
     } catch (error) {
       next(error);
     }
@@ -23,7 +23,17 @@ router.post('/recovery', async (req, res, next) => {
   try {
     const { email } = req.body;
     const rta = await service.sendRecovery(email);
-    res.json(rta)
+    res.json(rta);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/change-password', async (req, res, next) => {
+  try {
+    const { token, newPassword } = req.body;
+    const rta = await service.changePassword(token, newPassword);
+    res.json(rta);
   } catch (error) {
     next(error);
   }
