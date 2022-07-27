b54e6600902ae4faf035515b5528198f683811c7
davalder <davalder@hotmail.com>
2022-07-26 23:52:47 -0500

    se retira autenticacion para crear user

--- routes/users.router.js
+++ routes/users.router.js
@@ -36,7 +36,7 @@ router.get(
 
 router.post(
   '/',
-  passport.authenticate('jwt', { session: false }),
+  // passport.authenticate('jwt', { session: false }),
   validatorHandler(createUserSchema, 'body'),
   async (req, res, next) => {
     try {
